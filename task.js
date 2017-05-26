77fd6e8b84f1599f9a7aaf27af1feebf5c54cff6
mm1krob <mmikrob@gmail.com>
2017-05-26 15:41:36 +0300

    improved task
    забув доробити обробку багатьох параметрів в ...point, доробив. добавив коменти

--- task.js
+++ task.js
@@ -1,5 +1,6 @@
+//classes + inheritance + super
 	class Fighter{
-	constructor(name='anonymous', power=10, health=100){
+	constructor(name='anonymous', power=10, health=100){ //default parameters
 		this.name=name;
 		this.power=power;
 		this.health=health;
@@ -18,18 +19,23 @@ class ImprovedFighter extends Fighter{
 		super.hit(enemy,point*2);
 	}
 }
-let fighter= new Fighter('misha', 10, 1000);
-let improvedFighter= new ImprovedFighter('marta', 10,1000);
+let fighter= new Fighter('misha', 10, 1000); //block scoping (let)
+let improvedFighter= new ImprovedFighter('marta', 10,1000);//block scoping (let)
 
-let fight=(fighter, improvedFighter, ...point)=>{
+let fight=(fighter, improvedFighter, ...point)=>{ //arrow function and  spread / rest 
 	while(fighter.health>0&&improvedFighter.health>0){
-	fighter.hit(improvedFighter, point);
-	improvedFighter.doubleHit(fighter,point);
+	
+	let i =Math.floor(Math.random()*(point.length));//random point for figther
+	let k =Math.floor(Math.random()*(point.length));//random point for improvedfighter
+	fighter.hit(improvedFighter, point[k]);
+	improvedFighter.doubleHit(fighter,point[i]);
 	}
 	if (fighter.health>improvedFighter.health)
 		console.log(`***********************
-		${fighter.name} WON`);
+		${fighter.name} WON`);//string interpolation
 	else console.log(`***********************
-		${improvedFighter.name} WON`);
+		${improvedFighter.name} WON`);//string interpolation
 }
-fight(fighter, improvedFighter, 10)
+
+
+fight(fighter, improvedFighter, 20,2,50);
